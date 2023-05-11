cbe65abde4c2679d4689a59e71dea1d857ebf1f7
Romero <A127957@bf.arg.igrupobbva>
2023-05-11 16:31:24 -0300

    se agrega iframe

--- js/app.js
+++ js/app.js
@@ -1,38 +1,67 @@
 //funcion para obtener los valores desde una url del dolar
-function getURL(){
-    if (sessionStorage.getItem("dolarJSON") !=null){
-        dolarBlue=parseFloat(JSON.parse(sessionStorage.getItem("dolarJSON")))    
-    } else {$.get(URLGET, function (respuesta, estado) {
-        if(estado === "success"){
-          let misDatos = respuesta;          
-          let dolar=misDatos[1];//en esta posicion esta el valor del dolar que quiero obtener         
-          dolarBlue=dolar.casa.venta; //y de ese dolar blue elijo el valor de venta, que lo utilizo para hacer el calculo contra pesos argentinos       
-          sessionStorage.setItem("dolarJSON",JSON.stringify(dolarBlue));//guardo en mi session storage el valor
-        }});
-    }    
-}
+// function getURL(){
+//     if (sessionStorage.getItem("dolarJSON") !=null){
+//         dolarBlue=parseFloat(JSON.parse(sessionStorage.getItem("dolarJSON")))    
+//     } else {$.get(URLGET, function (respuesta, estado) {
+//         if(estado === "success"){
+//           let misDatos = respuesta;          
+//           let dolar=misDatos[1];//en esta posicion esta el valor del dolar que quiero obtener         
+//           dolarBlue=dolar.casa.venta; //y de ese dolar blue elijo el valor de venta, que lo utilizo para hacer el calculo contra pesos argentinos       
+//           sessionStorage.setItem("dolarJSON",JSON.stringify(dolarBlue));//guardo en mi session storage el valor
+//         }});
+//     }    
+// }
 
-const URLGET = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
-let dolarJSON=[];
-let dolarBlue =Number();
-getURL()
+// const URLGET = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
+// let dolarJSON=[];
+// let dolarBlue =Number();
+// getURL()
+// let precioGodOfWar = 70
+// let precioHarry = 60
+// let precioHorizon = 50
+// let valorGow= $("#valorGodOfWar")
+// let valorHarry=$("#valorHowgarts")
+// let valorHorizon=$("#valorHorizonFW")
+
+// precioGodOfWar = precioGodOfWar*dolarBlue
+// precioHarry = precioHarry*dolarBlue
+// precioHorizon = precioHorizon*dolarBlue
+
+// console.log("este es el precio del dolar blue " + dolarBlue)
+// console.log("este es el valor del god of war: "+ precioGodOfWar)
+// console.log("este es el valor de harry : "+precioHarry)
+// console.log("este es el valor de horizon: "+precioHorizon)
+
+// valorGow.append(precioGodOfWar)
+// valorHarry.append(precioHarry)
+// valorHorizon.append(precioHorizon)
+
+
 let precioGodOfWar = 70
 let precioHarry = 60
 let precioHorizon = 50
-let valorGow= $("#valorGodOfWar")
-let valorHarry=$("#valorHowgarts")
-let valorHorizon=$("#valorHorizonFW")
+let valorGow= document.getElementById("valorGodOfWar")
+let valorHarry=document.getElementById("valorHowgarts")
+let valorHorizon=document.getElementById("valorHorizonFW")
+let valores
 
-precioGodOfWar = precioGodOfWar*dolarBlue
-precioHarry = precioHarry*dolarBlue
-precioHorizon = precioHorizon*dolarBlue
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+.then(result=>result.json())
+.then(json=>{   
+    valores = json                
+    let dolar=valores[1];//en esta posicion esta el valor del dolar que quiero obtener         
+    let dolarBlue=parseInt(dolar.casa.venta); //y de ese dolar blue elijo el valor de venta, que lo utilizo para hacer el calculo contra pesos argentinos     
+    
+    precioGodOfWar = precioGodOfWar*dolarBlue
+    precioHarry = precioHarry*dolarBlue
+    precioHorizon = precioHorizon*dolarBlue
 
-console.log("este es el precio del dolar blue " + dolarBlue)
-console.log("este es el valor del god of war: "+ precioGodOfWar)
-console.log("este es el valor de harry : "+precioHarry)
-console.log("este es el valor de horizon: "+precioHorizon)
-
-valorGow.append(precioGodOfWar)
-valorHarry.append(precioHarry)
-valorHorizon.append(precioHorizon)
+    console.log("este es el precio del dolar blue " + dolarBlue)
+    console.log("este es el valor del god of war: "+ precioGodOfWar)
+    console.log("este es el valor de harry : "+precioHarry)
+    console.log("este es el valor de horizon: "+precioHorizon)
+    valorGow.innerHTML=`${precioGodOfWar}`
+    valorHarry.innerHTML=`${precioHarry}`
+    valorHorizon.innerHTML=`${precioHorizon}`
+}).catch(err=>console.log('Hubo un error'));
 
